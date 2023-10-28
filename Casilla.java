7a160a337475717196bb358ecdc4f71a813bbe8d
ev47 <134420722+ev47@users.noreply.github.com>
2023-10-27 21:57:32 -0600

    Casilla.java

--- Casilla.java
+++ Casilla.java
@@ -8,53 +8,50 @@ import javax.swing.*;
 import java.awt.Color;
 
 public class Casilla {
-    private int row;
-    private int column;
+    private int fila;
+    private int columna;
     JLabel label;
     
-    private boolean canChange = true;
+    private boolean puedecambiar=true;
     
-    public Casilla(int row, int column) {
-        this.row = row;
-        this.column = column;
+    public Casilla(int fila, int columna) {
+        this.fila = fila;
+        this.columna = columna;
         this.label = new JLabel();
         label.setBorder(BorderFactory.createLineBorder(Color.black));
         label.setOpaque(true);
     }
     
-    public int getRow() {
-        return row;
+    public int getFila() {
+        return fila;
     }
 
-    public int getColumn() {
-        return column;
+    public int getColumna() {
+        return columna;
     }
 
     public JLabel getLabel() {
         return label;
     }
     
-    public int getCurrentNumber() {
-        if (label.getText().equals("")) return -1;
-        
+    public int getnumeroActual() {
+        if (label.getText().equals("")) return -1;        
         return Integer.parseInt(label.getText());
     }
     
-    public void setCurrentNumber(int num) {
+    public void setnumeroActual(int num) {
         if (num == 0) {
             label.setText("");
             return;
-        }
-        
+        }        
         label.setText("" + num);
+    }  
+    public boolean puedeCambiar() {
+        return puedecambiar;
     }
     
-    public boolean canChange() {
-        return canChange;
-    }
-    
-    public void setChange(boolean canChange) {
-        this.canChange = canChange;
+    public void setPuedeCambiar(boolean puedecambiar) {
+        this.puedecambiar = puedecambiar;
     }
     
 }
