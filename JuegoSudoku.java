380a65a7813fd9eb14570190db95abf91282409a
ev47 <134420722+ev47@users.noreply.github.com>
2023-10-27 22:03:03 -0600

    JuegoSudoku.java

--- JuegoSudoku.java
+++ JuegoSudoku.java
@@ -18,35 +18,35 @@ import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 public class JuegoSudoku extends JFrame {
-    final int ROWS = 9;
-    final int COLUMNS = 9;
-    int numerosColocados = 0;
+      final int FILAS = 9;
+    final int COLUMNAS=9;
+    int numerosColocados=0;
 
     Casilla tablero[][];
     JPanel panelTablero;
 
-    ArrayList<Casilla> cuad1;
-    ArrayList<Casilla> cuad2;
-    ArrayList<Casilla> cuad3;
-    ArrayList<Casilla> cuad4;
-    ArrayList<Casilla> cuad5;
-    ArrayList<Casilla> cuad6;
-    ArrayList<Casilla> cuad7;
-    ArrayList<Casilla> cuad8;
-    ArrayList<Casilla> cuad9;
+    ArrayList<Casilla> cuadrante1;
+    ArrayList<Casilla> cuadrante2;
+    ArrayList<Casilla> cuadrante3;
+    ArrayList<Casilla> cuadrante4;
+    ArrayList<Casilla> cuadrante5;
+    ArrayList<Casilla> cuadrante6;
+    ArrayList<Casilla> cuadrante7;
+    ArrayList<Casilla> cuadrante8;
+    ArrayList<Casilla> cuadrante9;
 
     public JuegoSudoku() {
         // Instanciar arreglos
         // <editor-fold>
-        cuad1 = new ArrayList<Casilla>();
-        cuad2 = new ArrayList<Casilla>();
-        cuad3 = new ArrayList<Casilla>();
-        cuad4 = new ArrayList<Casilla>();
-        cuad5 = new ArrayList<Casilla>();
-        cuad6 = new ArrayList<Casilla>();
-        cuad7 = new ArrayList<Casilla>();
-        cuad8 = new ArrayList<Casilla>();
-        cuad9 = new ArrayList<Casilla>();
+        cuadrante1 = new ArrayList<Casilla>();
+        cuadrante2 = new ArrayList<Casilla>();
+        cuadrante3 = new ArrayList<Casilla>();
+        cuadrante4 = new ArrayList<Casilla>();
+        cuadrante5 = new ArrayList<Casilla>();
+        cuadrante6 = new ArrayList<Casilla>();
+        cuadrante7 = new ArrayList<Casilla>();
+        cuadrante8 = new ArrayList<Casilla>();
+        cuadrante9 = new ArrayList<Casilla>();
         
 
         // </editor-fold>
@@ -55,29 +55,27 @@ public class JuegoSudoku extends JFrame {
 
         panelTablero = new JPanel();
         add(panelTablero);
-        panelTablero.setLayout(new GridLayout(ROWS, COLUMNS));
-        tablero = new Casilla[ROWS][COLUMNS];
+        panelTablero.setLayout(new GridLayout(FILAS, COLUMNAS));
+        tablero = new Casilla[FILAS][COLUMNAS];
         llenarTablero();
         generarNumerosAleatorios();
         setVisible(true);
         setLocationRelativeTo(null);
     }
-
     private void guardarEnCuadrante(Casilla casilla) {
-        int row = casilla.getRow();
-        int col = casilla.getColumn();
-        ArrayList<Casilla> cuadrante = getCuadranteFromPos(row, col);
+        int fil = casilla.getFila();
+        int col = casilla.getColumna();
+        ArrayList<Casilla> cuadrante = getCuadranteFromPos(fil, col);
         cuadrante.add(casilla);
-
-        if (cuadrante == cuad1 || cuadrante == cuad3 || cuadrante == cuad5 || cuadrante == cuad7 || cuadrante == cuad9) {
+        if (cuadrante == cuadrante1 || cuadrante == cuadrante3 || cuadrante == cuadrante5 || cuadrante == cuadrante7 || cuadrante == cuadrante9) {
             casilla.getLabel().setBackground(Color.blue);
         }
 
     }
 
     private boolean esValidoFila(int num, int row) {
-        for (int c = 0; c < COLUMNS; c++) {
-            if (tablero[row][c].getCurrentNumber() == num) {
+        for (int c = 0; c < COLUMNAS; c++) {
+            if (tablero[row][c].getnumeroActual()== num) {
                 return false;
             }
         }
@@ -86,8 +84,8 @@ public class JuegoSudoku extends JFrame {
     }
 
     private boolean esValidoColumna(int num, int col) {
-        for (int r = 0; r < COLUMNS; r++) {
-            if (tablero[r][col].getCurrentNumber() == num) {
+        for (int r = 0; r < COLUMNAS; r++) {
+            if (tablero[r][col].getnumeroActual() == num) {
                 return false;
             }
         }
@@ -95,11 +93,11 @@ public class JuegoSudoku extends JFrame {
         return true;
     }
 
-    private Casilla getCasillaFromPos(int row, int col) {
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLUMNS; c++) {
+    private Casilla getCasillaPosicion(int row, int col) {
+        for (int r = 0; r < FILAS; r++) {
+            for (int c = 0; c < COLUMNAS; c++) {
                 Casilla casilla = tablero[r][c];
-                if (casilla.getRow() == row && casilla.getColumn() == col) {
+                if (casilla.getFila() == row && casilla.getColumna() == col) {
                     return casilla;
                 }
             }
@@ -108,44 +106,44 @@ public class JuegoSudoku extends JFrame {
         return null;
     }
 
-    private ArrayList<Casilla> getCuadranteFromPos(int row, int col) {
-        if (row <= 2) {
-            if (col <= 2) {
-                return cuad1;
-            } else if (col <= 5) {
-                return cuad2;
+    private ArrayList<Casilla> getCuadranteFromPos(int fila, int columna) {
+        if (fila <= 2) {
+            if (columna <= 2) {
+                return cuadrante1;
+            } else if (columna <= 5) {
+                return cuadrante2;
             } else {
-                return cuad3;
+                return cuadrante3;
             }
 
-        } else if (row <= 5) {
-            if (col <= 2) {
-                return cuad4;
+        } else if (fila <= 5) {
+            if (columna <= 2) {
+                return cuadrante4;
 
-            } else if (col <= 5) {
-                return cuad5;
+            } else if (columna <= 5) {
+                return cuadrante5;
             } else {
-                return cuad6;
+                return cuadrante6;
 
             }
         } else {
-            if (col <= 2) {
-                return cuad7;
+            if (columna <= 2) {
+                return cuadrante7;
 
-            } else if (col <= 5) {
-                return cuad8;
+            } else if (columna <= 5) {
+                return cuadrante8;
             } else {
-                return cuad9;
+                return cuadrante9;
 
             }
         }
     }
 
     private boolean esValidoCuadrante(int num, int row, int col) {
-        ArrayList<Casilla> cuad = getCuadranteFromPos(row, col);
+        ArrayList<Casilla> cuadran = getCuadranteFromPos(row, col);
 
-        for (Casilla casilla : cuad) {
-            if (casilla.getCurrentNumber() == num) {
+        for (Casilla casilla : cuadran) {
+            if (casilla.getnumeroActual()== num) {
                 return false;
             }
         }
@@ -156,27 +154,26 @@ public class JuegoSudoku extends JFrame {
         MouseAdapter mouseAdapter = new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-
-                Casilla casillaClickeada = null;
-                for (int r = 0; r < ROWS; r++) {
-                    for (int c = 0; c < COLUMNS; c++) {
+                Casilla casillapresionada = null;
+                for (int r = 0; r < FILAS; r++) {
+                    for (int c = 0; c < COLUMNAS; c++) {
                         if (tablero[r][c].getLabel() == e.getSource()) {
-                            casillaClickeada = tablero[r][c];
+                            casillapresionada = tablero[r][c];
                             break;
                         }
                     }
                 }
-                
-                if (casillaClickeada.canChange()) {
-                    administrarClick(casillaClickeada);
+          
+                if (casillapresionada.puedeCambiar()) {
+                    ControlarClick(casillapresionada);
                 } else {
                     JOptionPane.showMessageDialog(null, "No se puede cambiar el valor de las casillas prederminadas.", "ERROR", JOptionPane.ERROR_MESSAGE);
                 }    
             }
         };
 
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLUMNS; c++) {
+        for (int r=0;r < FILAS;r++) {
+            for (int c = 0; c < COLUMNAS; c++) {
                 tablero[r][c] = new Casilla(r, c);
                 tablero[r][c].getLabel().addMouseListener(mouseAdapter);
                 panelTablero.add(tablero[r][c].getLabel());
@@ -186,32 +183,29 @@ public class JuegoSudoku extends JFrame {
 
     }
 
-    private void administrarClick(Casilla casillaClickeada) {
+    private void ControlarClick(Casilla casillapresionada) {
         int n;
         do {
             
             try{
-                n = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero que desea anexar en esta casilla [Ingrese 0 para vaciar los contenidos]"));
-                
+                n= Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero que desea anexar en esta casilla [Ingrese 0 para vaciar los contenidos]"));                
                 if (n <= 0 || n >= 10) {
                     JOptionPane.showMessageDialog(null, "Error. Ingrese UNICAMENTE un numero entre 1 y 9");
 
                 }
                 
                 if (n == 0) {
-                    casillaClickeada.setCurrentNumber(n);
+                    casillapresionada.setnumeroActual(n);
                     return;
-                }
-                
-                
+                }                
             } catch (NumberFormatException e) {
                 JOptionPane.showMessageDialog(null, "Error. Ingrese UNICAMENTE un numero entre 1 y 9");
                 n = -1;
             }
         } while (n <= 0 || n>=10);
         
-        if (esNumeroValido(n, casillaClickeada.getColumn(), casillaClickeada.getRow())) {
-            casillaClickeada.setCurrentNumber(n);
+        if (esNumeroValido(n, casillapresionada.getColumna(), casillapresionada.getFila())) {
+            casillapresionada.setnumeroActual(n);
             numerosColocados++;
             verificarWin();
         } else {
@@ -234,7 +228,7 @@ public class JuegoSudoku extends JFrame {
         boolean esValido;
         
         // iterar cada columna y generar una cantidad aleatoria en esa columna
-        for (int c = 0; c < COLUMNS; c++) {
+        for (int j=0;j<COLUMNAS; j++) {
             System.out.println("Numeros pendientes por Generar: " + numerosPendientes);
             int cantidadNumerosEnColumna = random.nextInt(2, 5);
             
@@ -242,25 +236,25 @@ public class JuegoSudoku extends JFrame {
                 cantidadNumerosEnColumna = numerosPendientes;
             }
             
-            System.out.println("Cantidad de numeros a generar en columna " + c + ": " + cantidadNumerosEnColumna);
+            System.out.println("Cantidad de numeros a generar en columna " + j + ": " + cantidadNumerosEnColumna);
             System.out.println("Restante: " + (numerosPendientes - cantidadNumerosEnColumna));
             
             // Iterar n veces para generar la cantidad de numeros en esa columna
             for (int i = 0; i < cantidadNumerosEnColumna; i++) {
                 do {
-                    System.out.println("Generando #" + i + " para la columna: " + c);
+                    System.out.println("Generando #" + i + " para la columna: " + j);
                     int filaSeleccionada = random.nextInt(0, 9);
                     int numeroGenerado = random.nextInt(1, 10);
                     
-                    esValido = (esValidoCuadrante(numeroGenerado, filaSeleccionada, c)
+                    esValido = (esValidoCuadrante(numeroGenerado, filaSeleccionada, j)
                             && esValidoFila(numeroGenerado, filaSeleccionada)
-                            && esValidoColumna(numeroGenerado, c)) && tablero[filaSeleccionada][c].getCurrentNumber() == -1;
+                            && esValidoColumna(numeroGenerado, j)) && tablero[filaSeleccionada][j].getnumeroActual()== -1;
 
                     System.out.println((esValido) ?"VALIDO":"INVALIDO");
                     
                     if (esValido) {
-                        tablero[filaSeleccionada][c].setCurrentNumber(numeroGenerado);
-                        tablero[filaSeleccionada][c].setChange(false);
+                        tablero[filaSeleccionada][j].setnumeroActual(numeroGenerado);
+                        tablero[filaSeleccionada][j].setPuedeCambiar(false);
                         numerosPendientes--;
                         System.out.println("--------");
                         numerosColocados++; // contador para ver si el tablero esta lleno y determinar si se gano
@@ -276,12 +270,12 @@ public class JuegoSudoku extends JFrame {
         
         System.out.println("[generarNumerosAleatorios] Numeros que quedaron pendientes de colocar: " + numerosPendientes);
     }    
-    private void verificarWin() {
+    private void verificarGanador() {
         if (numerosColocados == 81) {            
-            JOptionPane.showMessageDialog(null, "¡Enhorabuena, monsieur/madame! Ganasteis el desafio de resolver el Sodoku.");
+            JOptionPane.showMessageDialog(null, "¡Enhorabuena, Ganastes el desafio de resolver el Sodoku.");
             dispose();
         }      
-    }
+    }  
     public static void main(String[] args) {
         new JuegoSudoku();
     }
